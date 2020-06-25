18e564aa9816f6d71e29ba9ded4e803fb52704bc
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-06-25 15:51:21 -0400

    Win functionality working

--- public/Javascript/script.js
+++ public/Javascript/script.js
@@ -5,17 +5,6 @@ let playerTwo = "O";
 let playerOneMoves = [];
 let playerTwoMoves = [];
 let playerTurn = playerOne;
-let wins = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 9]
-];
-let boardState = [false, false, false, false, false, false, false, false, false];
 let interval;
 let clockCount = 0;
 let moveCounter = 0;
@@ -25,47 +14,152 @@ let start = document.getElementById("start");
 let cells = Array.from(document.getElementsByClassName("cell"));
 let playerOneName = document.getElementById("playerOne");
 let playerTwoName = document.getElementById("playerTwo");
-let clock = document.getElementById("clock")
+let clock = document.getElementById("clock");
 
-// Need to check cells of player X and Player O and the Amount of moves to determine winner 
-
-//Need a function to count the moves of the players if 8 moves are counted and there is no winner, game results in a draw would need to add the count to an empty string to convert to an array
+//Every move adds to the moves and checks for a win. If it is more than 8 moves, declares a draw.
 function moves() {
-  moveCounter++
   if (moveCounter < 9) {
-    console.log(moveCounter)
-    //checkWin();
-  } else if (moveCounter = 9) {
-    statusArea.textContent = ("It's a Draw");
+    checkWin();
+  } else if ((moveCounter = 9)) {
+    statusArea.textContent = "It's a Draw";
     boardReset();
+  }
+  moveCounter++;
+}
+
+//Function that checks whether or not there is a win
+function checkWin() {
+  if (
+    cellone.textContent === "X" &&
+    celltwo.textContent === "X" &&
+    cellthree.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellfour.textContent === "X" &&
+    cellfive.textContent === "X" &&
+    cellsix.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellseven.textContent === "X" &&
+    celleight.textContent === "X" &&
+    cellnine.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellone.textContent === "X" &&
+    cellfour.textContent === "X" &&
+    cellseven.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    celltwo.textContent === "X" &&
+    cellfive.textContent === "X" &&
+    celleight.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellthree.textContent === "X" &&
+    cellsix.textContent === "X" &&
+    cellnine.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellone.textContent === "X" &&
+    cellfive.textContent === "X" &&
+    cellnine.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellthree.textContent === "X" &&
+    cellfive.textContent === "X" &&
+    cellseven.textContent === "X"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellone.textContent === "O" &&
+    celltwo.textContent === "O" &&
+    cellthree.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellfour.textContent === "" &&
+    cellfive.textContent === "O" &&
+    cellsix.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellseven.textContent === "O" &&
+    celleight.textContent === "O" &&
+    cellnine.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellone.textContent === "O" &&
+    cellfour.textContent === "O" &&
+    cellseven.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    celltwo.textContent === "O" &&
+    cellfive.textContent === "O" &&
+    celleight.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellthree.textContent === "O" &&
+    cellsix.textContent === "O" &&
+    cellnine.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellone.textContent === "O" &&
+    cellfive.textContent === "O" &&
+    cellnine.textContent === "O"
+  ) {
+    gameSatisfied();
+  } else if (
+    cellthree.textContent === "O" &&
+    cellfive.textContent === "O" &&
+    cellseven.textContent === "O"
+  ) {
+    gameSatisfied();
   }
 }
 
-//Function to begin the game
-start.addEventListener("click", () => {
+//A win has been satisfied. Alerts the game status area and resets the board
+function gameSatisfied() {
+  statusArea.textContent = playerTurn + " has won! Play again?";
+  boardReset();
+}
 
-  start.disabled = true;
-  displayStatusArea();
-  interval = setInterval(() => {
-    updateClock()
-  }, 1000)
-  cells.forEach((cell) => {
-    cell.addEventListener("click", clicked);
+//Function to begin the game
+function startFunction() {
+  start.addEventListener("click", () => {
+    start.disabled = true;
+    displayStatusArea();
+    interval = setInterval(() => {
+      updateClock();
+    }, 1000);
+    cells.forEach((cell) => {
+      cell.addEventListener("click", clicked);
+    });
   });
-});
+}
 
 //Function to add a mark to a cell when clicked
 function clicked(event) {
   event.target.textContent = playerTurn;
   event.target.removeEventListener("click", clicked);
-  event.target.addEventListener("click", clickedBefore)
+  event.target.addEventListener("click", clickedBefore);
+  
   if (playerTurn === playerOne) {
     playerTurn = playerTwo;
   } else playerTurn = playerOne;
   displayStatusArea();
   moves();
-
-
+  
 }
 
 //Function to alert the user that they have selected the cell before
@@ -87,21 +181,25 @@ function updateClock() {
 
 //Resets the Board
 function boardReset() {
-
-  boardState = [false, false, false, false, false, false, false, false, false]
+  boardState = [false, false, false, false, false, false, false, false, false];
 
   start.disabled = false;
-  start.textContent = 'Play Again!'
+  start.textContent = "Play Again!";
 
   start.addEventListener("click", () => {
-    removeEventListener()
-  })
+    removeEventListener();
+  });
+  startFunction();
+  moveCounter = 0;
+  playerTurn = playerOne;
 }
 
 function removeEventListener() {
   cells.forEach((cell) => {
-    cell.textContent = ""
-    cell.removeEventListener('click', clicked)
-    cell.removeEventListener('click', clickedBefore)
-  })
+    cell.textContent = "";
+    cell.removeEventListener("click", clicked);
+    cell.removeEventListener("click", clickedBefore);
+  });
 }
+
+startFunction();
