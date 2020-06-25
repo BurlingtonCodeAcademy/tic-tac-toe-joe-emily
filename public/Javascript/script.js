044a47901a59fe916427e52a3e6e8077fbf8b433
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-06-24 20:27:31 -0400

    Marking cells functionality, toggles between player turns.

--- public/Javascript/script.js
+++ public/Javascript/script.js
@@ -1,22 +1,36 @@
 //Global variable
 
-let playerOne="X";
-let playerTwo="O";
-let playerTurn=playerOne;
+let playerOne = "X";
+let playerTwo = "O";
+let playerTurn = playerOne;
+let start= document.getElementById("start");
+let cells = Array.from(document.getElementsByClassName("cell"))
+
 
 //Function to begin the game
-start.addEventListener("click", ()=>{
-    start.disabled= true;
-    displayStatusArea();
-})
+start.addEventListener("click", () => {
+  start.disabled = true;
+  displayStatusArea();
+  cells.forEach((cell)=>{
+      cell.addEventListener("click", clicked)
+  })
+});
 
-//Function to display status area
-function displayStatusArea(){
-    let statusArea= document.getElementById("statusArea");
-    statusArea.textContent= ("It is "+playerTurn+"'s turn!");
-    statusArea.appendChild(statusArea);
+//Function to add a mark to a cell when clicked
+function clicked(event){
+    event.target.textContent= playerTurn;
+    if(playerTurn===playerOne){
+        playerTurn = playerTwo
+    }else(playerTurn = playerOne)
+    
 }
 
+//Function to display status area
+function displayStatusArea() {
+  let statusArea = document.getElementById("statusArea");
+  statusArea.textContent = "It is " + playerTurn + "'s turn!";
+
+}
 
 
 
